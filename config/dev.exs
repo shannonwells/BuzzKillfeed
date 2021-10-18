cffc35a30b5bc57ae0d0b30842f6e27bf015623c
shannonwells <shannonwells@users.noreply.github.com>
2021-10-17 19:46:20 -0700

    mobile-first styling basics

--- config/dev.exs
+++ config/dev.exs
@@ -23,7 +23,7 @@ config :buzz_killfeed,
        # Binding to loopback ipv4 address prevents access from other machines.
        # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
        http: [
-         ip: {127, 0, 0, 1},
+         ip: {0, 0, 0, 0},
          port: 4000
        ],
        check_origin: false,
