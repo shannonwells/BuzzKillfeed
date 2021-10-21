6257b3624c83ea8b1780fb0e32a0810326f0f14d
Shannon Wells <shannonwells@users.noreply.github.com>
2021-10-20 18:11:15 -0700

    desktop styling improvements (#8)

--- lib/buzz_killfeed/seeds/template_seeds.ex
+++ lib/buzz_killfeed/seeds/template_seeds.ex
@@ -4,24 +4,24 @@ defmodule BuzzKillfeed.Seeds.TemplateSeeds do
 
   def seed() do
     templates = [
-      "Some {type_of_person_or_animal} Tried To {verb} Mount Everest. What Happened Next Has {adjective} {plural_person_or_animal} Rioting",
+      "Some {person_or_animal} Tried To {verb} Mount Everest. What Happened Next Has {adjective} {plural_person_or_animal} Rioting",
       "The {number} {adjective} {plural_noun} That {verb} Your {noun}",
-      "When {celebrity} {past_tense_verb} A {adjective} {noun}, I {past_tense_verb} my {noun}",
-      "You {adverb} Don't Want to {verb} This {adjective} {noun}",
+      "When {famous_figure} {past_tense_verb} A {adjective} {noun}, I Lost My {noun}",
       "This {adjective} {noun} That {past_tense_verb} A {noun} Can't Be Unseen",
-      "{person_name} Wants To {verb} Your {noun}. What Could Go Wrong?",
-      "{person_name} {adverb} Wishes Everyone Would Stop {verb}ing So {adjective}",
+      "{famous_figure} Wants To {verb} Your {noun}. What Could Go Wrong?",
+      "{famous_figure} {adverb} Wishes Everyone Would Stop {verb_+_ing} So {adverb}",
       "My {noun} Stopped {verb}ing, And Now I Can't Stop {verb}ing my {noun}",
-      "{celebrity} Says \"I'm {adverb} {adjective} I Missed My Big Chance To {verb} Before I {verb}\"",
+      "{famous_figure} Says \"I'm {adverb} {adjective} I Missed My Big Chance To {verb} Before I {verb}\"",
       "Poll: How Many Times Would You {verb} This {noun} If We Gave You {number} {plural_noun}?",
-      "It Might've Been {celebrity}'s {noun}, but {celebrity} Stole The Show With Their {adjective} {noun}",
+      "It Might've Been {famous_figure}'s {noun}, but {famous_figure} Stole The Show With Their {adjective} {noun}",
       "{number} {adjective} {noun} Tricks From A Former {noun} Painter",
       "Can {adjective} {noun} Discolor Your Underwear? We Asked {number} {plural_noun} for Answers",
-      "This {number} {time_period} Meditation From {celebrity} Had Me in {plural_noun} — in a Good Way",
+      "This {number} {time_period} Meditation From {famous_figure} Had Me in {plural_noun} — in a Good Way",
       "These {number} {adjective} {noun} {season} Dresses That'll Look {adjective} All The Way To {holiday}",
       "This {adjective} Hack Will {adverb} Change The Way You {verb} {plural_noun} - No Peeler Necessary!",
-      "{brand_name} Fitness Has Revived the Annual {adjective} {time_period} Workout Challenge — Our {plural_noun} Are {verb}ing Already",
-      "This {person} Stumbled Upon A {adjective} {noun} in {place}. What Happened Next Made Me -- And Millions of {plural_noun} -- Feel {adjective}",
+      "When In {place}, Never {verb} These {number} {adjective} Things, Or You'll Be {adjective}",
+      "{brand_name} Fitness Has Revived the Annual {number} {time_period} Workout Challenge — Our {plural_noun} Are {verb_+_ing} Already",
+      "This {type_of_person} Stumbled Upon A {adjective} {noun} in {place}. What Happened Next Made Me -- And Millions of {plural_noun} -- Feel {adjective}",
     ]
     |> Enum.map(fn v -> [value: v] end)
     Repo.insert_all(Template, templates, on_conflict: :nothing)
