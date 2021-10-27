bdf01d499193f003189782e7e972d628d2e773cf
shannonwells <shannonwells@users.noreply.github.com>
2021-10-27 15:30:55 -0700

    use conn assign in controllers

--- lib/buzz_killfeed_web/controllers/fill_in_the_bait_controller.ex
+++ lib/buzz_killfeed_web/controllers/fill_in_the_bait_controller.ex
@@ -4,12 +4,15 @@ defmodule FillInTheBait.FillInTheBaitController do
 
   def index(conn, _params) do
     conn
-    |> render("index.html", %{template: random_template() })
+    |> assign(:template,  random_template())
+    |> render("index.html")
   end
 
   def create(conn, %{"wordlist" => wordlist, "template_id" => template_id}) do
     template = get_template!(template_id).value
     conn
-    |> render("create.json", %{template: template, wordlist: wordlist})
+    |> assign(:template, template)
+    |> assign(:wordlist, wordlist)
+    |> render("create.json")
   end
 end
