d9181d22d601bbdfbb5997408212f69ef8efaf81
shannonwells <shannonwells@users.noreply.github.com>
2021-10-29 19:56:48 -0700

    fix mobile styling issues

--- config/dev.exs
+++ config/dev.exs
@@ -76,7 +76,7 @@ config :logger, :console, format: "[$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
+config :phoenix, :stacktrace_depth, 10
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
